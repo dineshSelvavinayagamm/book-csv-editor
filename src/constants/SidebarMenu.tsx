551730243440a387479751421cdc70f58a3b8f9e
ca-admin-repo <84333082+ca-admin-repo@users.noreply.github.com>
2024-09-30 21:36:00 +0530

    color update

--- src/constants/SidebarMenu.tsx
+++ src/constants/SidebarMenu.tsx
@@ -3,10 +3,11 @@
 import React from 'react';
 import { ValueIcon } from '@radix-ui/react-icons';
 
+// icon will support both URL or children
 export const SIDE_BAR_MENU = [
   {
     title: 'Home',
-    icon: 'https://img.icons8.com/?size=100&id=83326&format=png&color=000000',
+    icon: 'https://img.icons8.com/?size=100&id=83326&format=png&color=ffffff',
     items: [
       { title: 'Dashboard 1', route: '/', icon: <ValueIcon /> },
       { title: 'Dashboard 2', route: '/', icon: <ValueIcon /> },
@@ -14,7 +15,7 @@ export const SIDE_BAR_MENU = [
   },
   {
     title: 'Settings',
-    icon: 'https://img.icons8.com/?size=100&id=83326&format=png&color=000000',
+    icon: 'https://img.icons8.com/?size=100&id=83326&format=png&color=ffffff',
     items: [
       { title: 'Settings 1', route: '/', icon: <ValueIcon /> },
       { title: 'Settings 2', route: '/', icon: <ValueIcon /> },
@@ -22,7 +23,7 @@ export const SIDE_BAR_MENU = [
   },
   {
     title: 'About Us',
-    icon: 'https://img.icons8.com/?size=100&id=83326&format=png&color=000000',
+    icon: 'https://img.icons8.com/?size=100&id=83326&format=png&color=ffffff',
     route: '/',
   },
 ];
